df2eb6e24f9b6e9178a2b6729e0e751aad804e50
ComputerElectricElectronicJHLee <101415950+ComputerElectricElectronicJHLee@users.noreply.github.com>
2022-05-02 22:47:20 +0900

    [Silver III] Title: 1로 만들기, Time: 628 ms, Memory: 38652 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,15 @@
+import sys
+
+# a(i) = min(a(i-1),a(i//2),a(i//3))+1
+
+N = int(sys.stdin.readline().rstrip())
+
+memo = [0] * (N+1)
+
+for i in range(2,N+1):
+    memo[i] = memo[i-1]+1
+    if i % 2==0:
+        memo[i] = min(memo[i],memo[i//2]+1)
+    if i % 3==0:
+        memo[i] = min(memo[i],memo[i//3]+1)
+print(memo[N])
