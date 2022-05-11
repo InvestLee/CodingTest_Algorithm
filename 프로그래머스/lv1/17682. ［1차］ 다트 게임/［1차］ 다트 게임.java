3b90d22965f162ed4d8445a7926be4876540113b
ComputerElectricElectronicJHLee <101415950+ComputerElectricElectronicJHLee@users.noreply.github.com>
2022-05-12 01:31:07 +0900

    [level 1] Title: [1차] 다트 게임, Time: 9.80 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dartResult) {
+        int count = 0;
+        int [] cal = {0,0,0};
+        String temp = "";
+        for(int i = 0; i<dartResult.length(); i++){
+            if (Character.isDigit(dartResult.charAt(i))) temp += dartResult.charAt(i);
+            else{
+                if (temp.length() != 0){
+                    cal[count] = Integer.parseInt(temp);
+                    count++;
+                }
+                temp = "";
+                if (dartResult.charAt(i) == 'S') continue;
+                else if (dartResult.charAt(i) == 'D') cal[count-1] = cal[count-1] * cal[count-1];
+                else if (dartResult.charAt(i) == 'T') cal[count-1] = cal[count-1] * cal[count-1] * cal[count-1];
+                else if (dartResult.charAt(i) == '#') cal[count-1] *= -1;
+                else{
+                    if (count == 1) cal[count-1] *= 2;
+                    else{
+                        cal[count-1] *= 2;
+                        cal[count-2] *= 2;
+                    }
+                }
+            }
+        }
+        return Arrays.stream(cal).sum();
+    }
+}
