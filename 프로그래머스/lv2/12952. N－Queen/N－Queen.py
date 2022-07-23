07d86e5838633f6c5f32f93953a74f156875c87c
ComputerElectricElectronicJHLee <101415950+ComputerElectricElectronicJHLee@users.noreply.github.com>
2022-07-23 13:07:34 +0900

    [level 2] Title: N-Queen, Time: 1347.57 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.py
@@ -1,21 +1,15 @@
 def solution(n):
-    global result
-    result = 0
-    row = [0]*n
-    def check_possible(x):
-        for i in range(x):
-            if row[x] == row[i] or abs(row[x]-row[i]) == abs(x-i):
-                return False
-        return True
-    def N_Queen(x):
-        global result
-        if x == n:
-            result += 1
-            return
-        for i in range(n):
-            row[x] = i
-            if check_possible(x):
-                N_Queen(x+1)
-            
-    N_Queen(0)
-    return result
+    check_col = [False] * 100; check_d1 = [False] * 100; check_d2 = [False] * 100
+    def process(row):
+        answer = 0
+        if row == n+1:
+            return 1
+        for i in range(1,n+1):
+            d1 = row+i; d2 = n + (row - i)
+            if check_col[i] == False and check_d1[d1] == False and check_d2[d2] == False:
+                check_col[i] = True; check_d1[d1] = True; check_d2[d2] = True
+                answer += process(row+1)
+                check_col[i] = False; check_d1[d1] = False; check_d2[d2] = False
+        return answer
+    answer = process(1)
+    return answer
--- 프로그래머스/lv2/12952. N－Queen/README.md
+++ 프로그래머스/lv2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 9577.54 ms
+메모리: 10.2 MB, 시간: 1347.57 ms
 
 ### 구분
 
