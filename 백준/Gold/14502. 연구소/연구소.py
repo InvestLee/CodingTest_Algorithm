8cf8930c8a07c176463b3e9ac54c91f3a2a85d05
ComputerElectricElectronicJHLee <101415950+ComputerElectricElectronicJHLee@users.noreply.github.com>
2022-07-28 09:55:11 +0900

    [Gold IV] Title: 연구소, Time: 1324 ms, Memory: 118988 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,59 @@
+import sys
+from collections import deque
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+N, M = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+max_safety = 0
+
+#벽의 갯수
+wall_count = 3
+for i in range(N):
+    wall_count += graph[i].count(1)
+
+#바위러스 위치
+virus = []
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 2:
+            virus.append((i,j))
+
+#최대 안전 영역 계산 bfs
+def bfs():
+    global max_safety
+    visited = [[False for _ in range(M)] for _ in range(N)]
+    count = 0
+    q = deque()
+    for v in virus:
+        q.append(v)
+        visited[v[0]][v[1]] = True
+        count += 1
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+            if visited[nx][ny] == False and graph[nx][ny] == 0:
+                q.append((nx,ny))
+                visited[nx][ny] = True
+                count += 1
+    max_safety = max(max_safety,N*M-count-wall_count)
+
+#벽 3개 세우고 bfs돌리기
+def wall(cnt):
+    if cnt == 3:
+        bfs()
+        return
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                wall(cnt+1)
+                graph[i][j] = 0
+
+wall(0)
+print(max_safety)
