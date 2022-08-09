045388b2d7d05d7963782f8528892f9a370995c3
ComputerElectricElectronicJHLee <101415950+ComputerElectricElectronicJHLee@users.noreply.github.com>
2022-08-09 18:25:33 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 424.01 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,5 +1,10 @@
 def solution(board):
-    answer = 1 if 1 in board[0] else 0
+    #주워진 행렬이 1xn이거나 nx1인 경우
+    answer = 0
+    for board_row in board:
+        for b in board_row:
+            if b > answer:
+                answer = b
     #사각형 4개의 영역이 모두 1이면 오른쪽 끝 부분에 최솟값을 더해준다.
     for i in range(1,len(board)):
         for j in range(1,len(board[0])):
