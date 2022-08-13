20f6a4d0f99842dc5d8912af02220eae12f3023e
ComputerElectricElectronicJHLee <101415950+ComputerElectricElectronicJHLee@users.noreply.github.com>
2022-08-13 14:48:22 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 220 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] chess = new int[6];
+        int[] full = {1,1,2,2,2,8};
+        for (int i = 0; i < 6; i++) {
+            chess[i] = sc.nextInt();
+        }
+        sc.close();
+        for (int i = 0; i < 6; i++) {
+            System.out.printf("%d ",full[i]-chess[i]);
+        }
+    }
+}
