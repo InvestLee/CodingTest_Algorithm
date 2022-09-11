71d1a5736640a137659e0b030206eb150c54e783
ComputerElectricElectronicJHLee <101415950+ComputerElectricElectronicJHLee@users.noreply.github.com>
2022-09-11 19:17:20 +0900

    [Bronze V] Title: 사파리월드, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger N = new BigInteger(st.nextToken());
+        BigInteger M = new BigInteger(st.nextToken());
+        br.close();
+        System.out.println(N.subtract(M).abs());
+    }
+}
