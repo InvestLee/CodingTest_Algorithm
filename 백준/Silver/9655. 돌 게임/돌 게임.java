7e9539d72f2463cc2b48d36b52aa8eb6b9715afa
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2022-11-19 09:09:32 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n % 2 == 1) System.out.println("SK");
+        else System.out.println("CY");
+    }
+}
