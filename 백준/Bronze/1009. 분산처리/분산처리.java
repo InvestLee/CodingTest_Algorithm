3ac33f0684c3e4ec93c515429b3757a4a1c590ac
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2022-12-21 00:14:58 +0900

    [Bronze II] Title: 분산처리, Time: 1016 ms, Memory: 15572 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int result = 1;
+            for (int j = 0; j < b; j++){
+                result = (result*a)%10;
+            }
+            if(result == 0) result = 10;
+            System.out.println(result);
+        }
+    }
+}
