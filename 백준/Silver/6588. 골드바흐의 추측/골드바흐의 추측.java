80faf682d83ddd051ff12adbcfd8953605daa108
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2023-02-14 06:38:59 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1368 ms, Memory: 38852 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,33 +1,43 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
+
+    public static int r,c,n;
+    public static char[][] graph;
+    public static Queue<int[]> q = new LinkedList<>();
+    public static int[] dx = {-1,1,0,0};
+    public static int[] dy = {0,0,-1,1};
+
     public static void main(String[] args) throws IOException {
-        boolean[] visited = new boolean[1000001];
-        for (int i = 2; i < (int) Math.sqrt(visited.length) + 1; i++){
-            if (!visited[i]) {
-                int j = 2;
-                while (i * j < visited.length) {
-                    visited[i * j] = true;
-                    j++;
-                }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n, j;
+        boolean isGoldbach;
+        boolean[] isPrime = new boolean[1000001];
+        isPrime[0] = isPrime[1] = true;
+        for (int i = 2; i <= (int) Math.sqrt(1000001); i++){
+            j = 2;
+            while (i*j<1000001) {
+                isPrime[i * j] = true;
+                j++;
             }
         }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true){
-            int n = Integer.parseInt(br.readLine());
-            if (n == 0) break;
-            boolean goldbach = false;
-            for (int i = 3; i < n/2+1; i++){
-                if (!visited[i] && !visited[n-i]){
-                    goldbach = true;
-                    System.out.printf("%d = %d + %d\n", n, i, n-i);
+            n = Integer.parseInt(br.readLine());
+            if (n==0) break;
+            isGoldbach = false;
+            for (int i = 2; i <= n/2; i++){
+                if(!isPrime[i] && !isPrime[n-i]){
+                    System.out.println(n + " = " + i + " + " + (n - i));
+                    isGoldbach = true;
                     break;
                 }
             }
-            if (!goldbach) System.out.println("Goldbach's conjecture is wrong.");
+            if (!isGoldbach) System.out.println("Goldbach's conjecture is wrong.");
         }
+
     }
 }
