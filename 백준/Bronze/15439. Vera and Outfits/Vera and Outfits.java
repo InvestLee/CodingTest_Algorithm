8c435b509c388fa6c237c928b16ac5686d0a86bd
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2023-02-17 06:59:00 +0900

    [Bronze IV] Title: Vera and Outfits, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. Vera and Outfits/Vera and Outfits.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n * (n-1));
+    }
+}
