7bfc2dce30c9a6f4753c63ed1d49d19a3e415872
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2023-04-07 22:23:08 +0900

    [Bronze IV] Title: 녹색거탑, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((int) Math.pow(2,n));
+    }
+}
