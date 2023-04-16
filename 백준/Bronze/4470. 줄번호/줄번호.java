d93140fcd7b818e2754466c53409ad059926eff7
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2023-04-17 06:37:16 +0900

    [Bronze IV] Title: 줄번호, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) System.out.printf("%d. %s\n",i+1,br.readLine());
+    }
+}
