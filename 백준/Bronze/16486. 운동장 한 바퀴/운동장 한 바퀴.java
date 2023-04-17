6c43e795f9bde8ed537319ccfc5e8820f9e69ec6
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2023-04-18 06:39:42 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int d1 = Integer.parseInt(br.readLine());
+        int d2 = Integer.parseInt(br.readLine());
+        System.out.println(2*d1+2*d2*3.141592);
+    }
+}
