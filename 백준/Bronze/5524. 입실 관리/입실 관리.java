86db8cc383998e8f92fe68bb894d53cccf0196f8
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2023-05-03 07:06:15 +0900

    [Bronze IV] Title: 입실 관리, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String enterRecord;
+        while(n-- > 0){
+            enterRecord = br.readLine();
+            System.out.println(enterRecord.toLowerCase());
+        }
+    }
+}
