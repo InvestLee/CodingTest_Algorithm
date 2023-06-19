cef83762768a629a578cd18edb524adb4b3f3fb8
ComputerElectricElectronicJHLee <dlwlgud216@nate.com>
2023-06-20 07:10:50 +0900

    [Bronze IV] Title: 이칙연산, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/15726. 이칙연산/이칙연산.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+        double c = Double.parseDouble(st.nextToken());
+
+        bw.write((int)Math.max(a*b/c,a/b*c)+"");
+        bw.flush();
+        bw.close();
+    }
+}
