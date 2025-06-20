c3defb9b06e017883f6aded7f69c2a142c8e24e1
JHLee <dlwlgud216@nate.com>
2025-06-20 19:19:31 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num, int k) {
+        String str = String.valueOf(num);
+        for(int i = 0; i < str.length(); i++) if(str.charAt(i) - '0' == k) return i+1;
+        return -1;
+    }
+}
