b24aa3476e0f40c28ab7511887f7e81020b4884c
JHLee <dlwlgud216@nate.com>
2025-07-08 08:01:19 +0900

    [level 0] Title: qr code, Time: 12.38 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        for(int i = r; i < code.length(); i += q) answer += code.charAt(i);
+        return answer;
+    }
+}
