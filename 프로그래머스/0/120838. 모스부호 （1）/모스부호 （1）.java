021b73431b999d71218e859e48541c3850546300
JHLee <dlwlgud216@nate.com>
2025-07-25 21:32:51 +0900

    [level 0] Title: 모스부호 (1), Time: 1.56 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String letter) {
+        HashMap<String, String> morse = new HashMap<>();
+        morse.put(".-", "a");
+        morse.put("-...", "b");
+        morse.put("-.-.", "c");
+        morse.put("-..", "d");
+        morse.put(".", "e");
+        morse.put("..-.", "f");
+        morse.put("--.", "g");
+        morse.put("....", "h");
+        morse.put("..", "i");
+        morse.put(".---", "j");
+        morse.put("-.-", "k");
+        morse.put(".-..", "l");
+        morse.put("--", "m");
+        morse.put("-.", "n");
+        morse.put("---", "o");
+        morse.put(".--.", "p");
+        morse.put("--.-", "q");
+        morse.put(".-.", "r");
+        morse.put("...", "s");
+        morse.put("-", "t");
+        morse.put("..-", "u");
+        morse.put("...-", "v");
+        morse.put(".--", "w");
+        morse.put("-..-", "x");
+        morse.put("-.--", "y");
+        morse.put("--..", "z");
+        String[] arr = letter.split(" ");
+        String answer = "";
+        for(int i = 0; i < arr.length; i++) answer += morse.get(arr[i]);
+        return answer;
+    }
+}
